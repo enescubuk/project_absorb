75d425425b9d20088f32ec96391bfb0f87af9c5c
Enes <enescubuk10@gmail.com>
2023-01-27 00:13:56 +0300

    inventory

---
+++ project_absorb/Assets/Scripts/Game/ShopManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    public List<GameObject> shopAisle; // all item to be sold
+
+
+    void selectFourItem()
+    {
+        
+    }
+}
