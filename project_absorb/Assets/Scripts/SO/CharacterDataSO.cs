edd4e1b67e7e2710d1972b1b79e1338b3e98f772
Enes <enescubuk10@gmail.com>
2023-02-26 15:31:56 +0300

    characterdata

---
+++ project_absorb/Assets/Scripts/SO/CharacterDataSO.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character Data")]
+public class CharacterDataSO : ScriptableObject
+{
+    [SerializeField] private int level = 1;
+    [SerializeField] private int money = 0;
+    [SerializeField] private int health = 100;
+
+
+
+
+    public int Level { get { return level; } set { level = value; } }
+    public int Money { get { return money; } set { money = value; } }
+    public int Health { get { return health; } set { health = value; } }
+}
--- project_absorb/Assets/Scripts/SO/ItemSO.cs
+++ project_absorb/Assets/Scripts/SO/ItemSO.cs
@@ -6,12 +6,12 @@ using UnityEngine.EventSystems;
 public class ItemSO : MonoBehaviour
 {
     public CardValuesSO cardValuesSO;
-    public Text cardname_Text;
+    private Text cardname_Text => GetComponentInChildren<Text>();
     public Text carmanaCost_Text;
     void Awake()
     {
         cardname_Text.text = cardValuesSO.cardName;
-        carmanaCost_Text.text = cardValuesSO.cardMana.ToString();
+        //carmanaCost_Text.text = cardValuesSO.cardMana.ToString();
     }
 
 }
