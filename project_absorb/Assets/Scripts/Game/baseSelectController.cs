d84174670beeb78dcd3a79ccade82ade66d0a93a
Kadir Cem Yılmaz <73888204+pholsen@users.noreply.github.com>
2023-02-26 22:21:05 +0300

    yellowErrorFix

--- project_absorb/Assets/Scripts/Game/baseSelectController.cs
+++ project_absorb/Assets/Scripts/Game/baseSelectController.cs
@@ -13,7 +13,7 @@ public class baseSelectController : MonoBehaviour
     public GameObject textParents;
     public Image roomSprite;
     private GameObject targetRoom;
-    public Camera camera;
+    private new Camera camera;
     private Vector3 lastCameraPos;
     private float lastCameraSize;
     Vector3 firstPosSprite,firstPosTexts;
