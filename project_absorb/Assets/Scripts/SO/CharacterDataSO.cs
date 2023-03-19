081edd926472b0a3f564c289ed04fdc9a1fe8577
Kadir Cem Yılmaz <73888204+pholsen@users.noreply.github.com>
2023-03-19 03:18:23 +0300

    Hospital

--- project_absorb/Assets/Scripts/SO/CharacterDataSO.cs
+++ project_absorb/Assets/Scripts/SO/CharacterDataSO.cs
@@ -6,13 +6,11 @@ public class CharacterDataSO : ScriptableObject
     [SerializeField] private int level = 1;
     [SerializeField] private int money = 0;
     [SerializeField] private int health = 100;
-<<<<<<< Updated upstream
-=======
+
 
     [SerializeField] private int maxHealth = 100;
     [SerializeField] private int xp = 100;
 
->>>>>>> Stashed changes
 
 
 
@@ -20,9 +18,7 @@ public class CharacterDataSO : ScriptableObject
     public int Level { get { return level; } set { level = value; } }
     public int Money { get { return money; }  set { money = value; } }
     public int Health { get { return health; } set { health = value; } }
-<<<<<<< Updated upstream
-=======
     public int MaxHealth { get { return maxHealth; } set { maxHealth = value; } }
     public int Xp { get { return xp; } set { xp = value; } }
->>>>>>> Stashed changes
+
 }
