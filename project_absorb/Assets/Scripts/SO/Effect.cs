a1332a7e4fd72e2192250b2216a5605d06524418
Enes <enescubuk10@gmail.com>
2023-04-09 02:42:13 +0300

    enemy effect icon

--- project_absorb/Assets/Scripts/SO/Effect.cs
+++ project_absorb/Assets/Scripts/SO/Effect.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Effect : MonoBehaviour
 {
@@ -8,28 +10,40 @@ public class Effect : MonoBehaviour
     public EffectSO effectSO;
 
     public int duration;
+    public TMP_Text effectCounter;
+    public Image effectSprite;
     void Awake()
     {
         
     }
+    void changeValues(string text, bool sprite)
+    {
+        effectCounter.text = text;
+        effectSprite.enabled = sprite;
+    }
 
-    private void Start() {
-        
-        
+    private void Start() 
+    {
+        effectCounter = gameObject.transform.Find("EffectCounter").GetComponentInChildren<TMP_Text>();
+        effectSprite = gameObject.transform.Find("Effect").GetComponentInChildren<Image>();
+        changeValues(duration.ToString(), true);
         if (GetComponent<Effect>() == this)
         {
             effectSO.currentDamage = 1;
         }
+        effectCounter.text = duration.ToString();
     }
     public void EffectEnemy()
     {
         
         if (duration < 1)
         {
+            changeValues(" ",false);
             Destroy(this);
         }
         GetComponent<EnemyScript>().hp -= duration;
         duration--;
+        changeValues(duration.ToString(), true);
     }
 
     public void EffectPlayer()
@@ -37,9 +51,11 @@ public class Effect : MonoBehaviour
         GameManager.current.playerHp -= effectSO.effectDamage;
         if (duration <= 0)
         {
+            changeValues(" ",false);
             Destroy(this);
         }
         duration--;
+        changeValues(duration.ToString(), true);
     }
 
 }
